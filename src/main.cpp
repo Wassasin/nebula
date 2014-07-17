99344968135137c0875a2b587e0f0177fd630db4
Wouter Geraedts <git@woutergeraedts.nl>
2014-07-17 10:05:12 +0200

    main: moved debug_lines from comment to function

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,28 @@
 #include "glfwcontext.hpp"
 #include "glutcontext.hpp"
 #include "simplescene.hpp"
+#include "nebulascene.hpp"
 #include "nebulaparticlescene.hpp"
+
+void add_debug_lines(rendercontext& r)
+{
+	r.add_cb(rcphase::draw, [](rendercontext& s) {
+		glLineWidth(2.0);
+		glBegin(GL_LINES);
+		for(qi i = 0; i < 3; i++)
+		{
+			glColor3f(i == 0, i == 1, i == 2);
+			glVertex3f(0.0, 0.0, 0.0);
+			glVertex3f(i == 0, i == 1, i == 2);
+		}
+		glEnd();
+	});
+}
 
 int main(int argc, char** argv)
 {
 	glfwcontext r;
-	//simplescene s(r); // Context for scene
 	nebulaparticlescene s(r);
-
-	/*r.add_cb(rcphase::draw, [](rendercontext& s) {
-			glLineWidth(2.0);
-			glBegin(GL_LINES);
-			for(qi i = 0; i < 3; i++)
-			{
-				glColor3f(i == 0, i == 1, i == 2);
-				glVertex3f(0.0, 0.0, 0.0);
-				glVertex3f(i == 0, i == 1, i == 2);
-			}
-			glEnd();
-		});*/
 
 	r.run(argc, argv);
 }
